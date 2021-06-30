c60924db41b5821352a93c404ed5dcdd6e389627
liqi <liqi@gmail.com>
2021-06-30 22:26:03 +0800

    commit

--- springsource-test/src/main/java/com/atguigu/spring/bean/Person.java
+++ springsource-test/src/main/java/com/atguigu/spring/bean/Person.java
@@ -11,6 +11,14 @@ public class Person {
 	private String name;
 	private int age;
 
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	@Override
 	public String toString() {
 		return "Person{" +
